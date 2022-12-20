902f4c17ec31dbf98d7bf6d07b2e6d1755f8428d
Evgeni <Kremeni@gmail.com>
2022-12-20 14:53:04 +0300

    HomeWork-1

---
+++ core-service/src/main/java/ru/kremenia/market/core/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package ru.kremenia.market.core.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+import ru.kremenia.market.core.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+
+}
